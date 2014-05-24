d01e9eb27f77a87e93df19f5e629861fc287ea1f
mbrenman <mattbrenman@gmail.com>
2014-05-24 15:43:26 -0400

    camera follows ball

---
+++ Bowling/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject _target;
+	public Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+		_target = GameObject.Find ("Ball"); // Follow the ball
+		_offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = _target.transform.position + _offset;
+	}
+}
