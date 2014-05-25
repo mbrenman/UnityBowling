454aa1dced1a237860fe5a9bbe915987772cf757
mbrenman <mattbrenman@gmail.com>
2014-05-25 19:23:01 -0400

    ball moves forward now on start until throw starts -- change to side to side

--- Bowling/Assets/Scripts/BallThrower.cs
+++ Bowling/Assets/Scripts/BallThrower.cs
@@ -6,19 +6,28 @@ public class BallThrower : MonoBehaviour {
 	public float _power = 0;
 	public float _throw = 20;
 	public bool thrown = false;
+	public bool throwHasStarted = false;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Space)) {
+			throwHasStarted = true;
 			_power += _throw * Time.deltaTime;
 		}
 		if (Input.GetKeyUp (KeyCode.Space)) {
 			StartCoroutine(ThrowBall());		
+		}
+		SetUpBallLocation ();
+	}
+
+	void SetUpBallLocation() {
+		if (!throwHasStarted) {
+			Vector3 offset = new Vector3(0, 0, 0.05f);
+			this.transform.position += offset;
 		}
 	}
 
