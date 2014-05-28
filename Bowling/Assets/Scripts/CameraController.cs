04b2187f18493ea061f9c73471c6810e5a24a546
mbrenman <mattbrenman@gmail.com>
2014-05-27 23:27:26 -0400

    camera now stops on collision with pins

--- Bowling/Assets/Scripts/CameraController.cs
+++ Bowling/Assets/Scripts/CameraController.cs
@@ -6,16 +6,20 @@ public class CameraController : MonoBehaviour {
 	public GameObject _target;
 	public int _rotationOffset = -3;
 	public Vector3 verticalOffset = new Vector3(0f, 0f, 0f);
+	BallThrower ballScript;
 
 	// Use this for initialization
 	void Start () {
 		_target = GameObject.Find ("Ball"); // Follow the ball
+		ballScript = _target.GetComponent<BallThrower> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = _target.transform.rotation;
-		transform.position = _target.transform.position + (transform.forward * _rotationOffset);
-		transform.position += new Vector3 (0, 1, 0);
+		if (!ballScript._hittingPins) {
+			transform.rotation = _target.transform.rotation;
+			transform.position = _target.transform.position + (transform.forward * _rotationOffset);
+			transform.position += new Vector3 (0, 1, 0);
+		}
 	}
 }
