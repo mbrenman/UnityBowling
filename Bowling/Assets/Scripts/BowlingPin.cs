dbbe86f402b6fe8022316bcacc86a5557ea8a4eb
Souradeep-Nanda <souradeep_nanda@hotmail.com>
2014-06-05 14:14:12 +0530

    Upgraded the bowling pins
    Added new model, texture and placeholder sounds.

---
+++ Bowling/Assets/Scripts/BowlingPin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class BowlingPin : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		//Debug.Log(collision.relativeVelocity.magnitude);
+		if (collision.relativeVelocity.magnitude > 2){
+			audio.Play();
+		}
+		
+	}
+}
