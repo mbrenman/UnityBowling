1b31bcb90113d4a6738cbaea4efbb0f71b80d796
Souradeep-Nanda <souradeep_nanda@hotmail.com>
2014-06-06 14:58:41 +0530

    Made it smart phone ready
    Tested on android.
    It will run on computer too without any conflict.

--- Bowling/Assets/Scripts/BallThrower.cs
+++ Bowling/Assets/Scripts/BallThrower.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BallThrower : MonoBehaviour {
-
+public class BallThrower : MonoBehaviour
+{
 	public float _throw = 20;
 	public bool _hittingPins = false;
 	public float maxPower = 10f;
-
 	private float _power = 0;
 	private bool thrown = false;
 	private bool changingLocation = true;
@@ -18,87 +17,99 @@ public class BallThrower : MonoBehaviour {
 	private int maxRotation = 30;
 	private int minRotation = 330;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	private bool isTouched = false;
+	private bool isReleased = false;
 
-//		if (Input.touchCount == 1) {
-//			Touch touch = Input.GetTouch(0);
-			if (changingLocation) {
-				if (Input.GetKey (KeyCode.Space)) {
-//				if (touch.phase == TouchPhase.Began) {
-					changingLocation = false;
-				}
-				ChangeBallLocation ();
-			} else if (changingRotation) {
-				if (Input.GetKey (KeyCode.Space)) {
-//				if (touch.phase == TouchPhase.Began) {
-					changingRotation = false;
-				}
-				changeBallRotation();
-			} else {
-				if (Input.GetKey (KeyCode.Space)) {
-//				if (touch.phase == TouchPhase.Began) {
-					_power += _throw * Time.deltaTime;
-				}
-			if (Input.GetKeyUp (KeyCode.Space)) {
-//				if (touch.phase == TouchPhase.Ended) {
-					_power = Mathf.Min(_power,maxPower);
-					StartCoroutine(ThrowBall());
-				}
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		if(Input.touchCount>0 || Input.GetKey(KeyCode.Space)){
+			isTouched = true;
+		} else {
+			if(isTouched)
+				isReleased = true;
+			isTouched = false;
+		}
+		if (changingLocation) {
+			if (isTouched) {
+				//				if (touch.phase == TouchPhase.Began) {
+				changingLocation = false;
+				Debug.Log ("Location locked");
 			}
-//		}
+			ChangeBallLocation ();
+		} else if (changingRotation) {
+			if (isTouched) {
+				//				if (touch.phase == TouchPhase.Began) {
+				changingRotation = false;
+				Debug.Log ("Rotation locked");
+			}
+			changeBallRotation ();
+			} else {
+			if (isTouched) {
+				//			if (touch.phase == TouchPhase.Began) {
+				_power += _throw * Time.deltaTime;
+				//Debug.Log ("Adding power");
+			}
+			if (isReleased) {
+				//				if (touch.phase == TouchPhase.Ended) {
+				_power = Mathf.Min (_power, maxPower);
+				StartCoroutine (ThrowBall ());
+				Debug.Log ("Throwing with power: "+_power);
+			}
+		}
 	}
-
-	void ChangeBallLocation() {
-		Vector3 offset = new Vector3(0.04f, 0, 0);
+	void ChangeBallLocation ()
+	{
+		Vector3 offset = new Vector3 (0.04f, 0, 0);
 		if (increasingWidth) {
 			this.transform.position += offset;
-			if (this.transform.position.x > maxLaneWidth) {	
+			if (this.transform.position.x > maxLaneWidth) {
 				increasingWidth = false;
 				this.transform.position -= offset;
 			}
 		} else if (!increasingWidth) {
 			this.transform.position -= offset;
-			if (this.transform.position.x < minLaneWidth) {	
+			if (this.transform.position.x < minLaneWidth) {
 				increasingWidth = true;
 				this.transform.position += offset;
 			}
 		}
 	}
-
-	void changeBallRotation() {
+	void changeBallRotation ()
+	{
 		if (increasingRotation) {
-			transform.rotation = transform.rotation * Quaternion.Euler(0f, 1f, 0f);
-//			Debug.Log(transform.rotation.eulerAngles.y);
-			if (transform.rotation.eulerAngles.y > maxRotation && transform.rotation.eulerAngles.y < 60) {	
+			transform.rotation = transform.rotation * Quaternion.Euler (0f, 1f, 0f);
+			//			Debug.Log(transform.rotation.eulerAngles.y);
+			if (transform.rotation.eulerAngles.y > maxRotation && transform.rotation.eulerAngles.y < 60) {
 				increasingRotation = false;
 			}
 		} else if (!increasingRotation) {
-			transform.rotation = transform.rotation * Quaternion.Euler(0f, (-1f), 0f);
-			if (transform.rotation.eulerAngles.y < minRotation && transform.rotation.eulerAngles.y > 300) {	
+			transform.rotation = transform.rotation * Quaternion.Euler (0f, (-1f), 0f);
+			if (transform.rotation.eulerAngles.y < minRotation && transform.rotation.eulerAngles.y > 300) {
 				increasingRotation = true;
 			}
 		}
 	}
-
-	void OnCollisionEnter(Collision collision) {
+	void OnCollisionEnter (Collision collision)
+	{
 		if (collision.collider.gameObject.name == "Cylinder") {
 			_hittingPins = true;
 		}
 	}
-	
-	public IEnumerator ThrowBall() {
+
+	public IEnumerator ThrowBall ()
+	{
 		if (!thrown) {
 			//Do not throw anymore
-			thrown = true; 
+			thrown = true;
 			//Rotate the throw by the rotation angle
 			rigidbody.velocity = transform.forward * _power;
-			yield return new WaitForSeconds(5);
-			Application.LoadLevel(Application.loadedLevel);
+			yield return new WaitForSeconds (5);
+			Application.LoadLevel (Application.loadedLevel);
 		}
 	}
 }
